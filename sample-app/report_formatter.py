2d0c84d759d31783afa5105279baa3e15bc11be8
NIHAL-TP <nihaltp101@gmail.com>
2026-09-04 09:26:36 +0000

    completed report formatter,which converts our findings to a markdown file

--- sample-app/report_formatter.py
+++ sample-app/report_formatter.py
@@ -6,17 +6,37 @@ import json
 def report_formatter(findings,count):
     header_line=[]
     seperator_line=[]
-    for i in findings:
-        for j in i:    
+    content=[]
+    table=[]
+    char = "|"
+    for line in findings:
+        #print(line)
+        for j in line:    
             header_line.append(j)
-            seperator_line.append("---|")
-        print(header_line)
-        print(seperator_line)
-
+            seperator_line.append("---")
+        combined_header = char + " | ".join(header_line) + char
+        combined_seperator = char +  " | ".join(seperator_line) + char
+        #print(header_line)
+        #print(seperator_line)
         break
-    combined = header_line + seperator_line
-    final_string = "\n".join(combined)
-    print(final_string)
+    #print(combined_header)
+    #print(combined_seperator)
+    for line in findings:
+        content_line=[]
+        for j in line:
+            content_line.append(line[j])
+        combined_content_line = char + " | ".join(content_line)+ char
+        content.append(combined_content_line)
+    combined_content = "\n".join(content)
+    #print(combined_content)
+    table.append(combined_header)
+    table.append(combined_seperator)
+    table.append(combined_content)
+    table = "\n".join(table)
+    table = f"**Summary:** {count['HIGH']} HIGH, {count['MEDIUM']} MEDIUM issues found" + "\n" + table
+    return table
+
+    
 
 if __name__ == "__main__" :
     json_path=sys.argv[1]
@@ -27,4 +47,4 @@ if __name__ == "__main__" :
         print("\n\n\n")
         for j in i:
             print(f"{j} : {i[j]}")"""
-    report_formatter(findings,severity_count)
+    print("findings in markdown \n",report_formatter(findings,severity_count))
