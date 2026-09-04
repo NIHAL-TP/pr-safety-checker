c032637790b7cfd9b58d70881c8f6d68d9994617
NIHAL-TP <nihaltp101@gmail.com>
2026-09-04 14:21:28 +0000

    created main.py to manage all functions at once

---
+++ sample-app/main.py
@@ -0,0 +1,21 @@
+from parser import load_trivy_results,extract_findings,count_by_severity
+from report_formatter import report_formatter
+from pr_commentor import post_comment
+import os
+import sys
+from dotenv import load_dotenv
+load_dotenv()
+
+if __name__ == "__main__":
+    REPO="pr-safety-checker"
+    ISSUE_NUMBER="1"
+    data=sys.argv[1]
+    trivy_data=load_trivy_results(data)
+    severity_count=count_by_severity(trivy_data)
+    findings=extract_findings(trivy_data)
+    formatted_report=report_formatter(findings,severity_count)
+    username=os.getenv("GITHUB_USER")
+    token=os.getenv("GITHUB_TOKEN")
+    api=f"https://api.github.com/repos/{username}/{REPO}/issues/{ISSUE_NUMBER}/comments"
+    headers = {"Authorization": f"Bearer {token}"}
+    response=post_comment(api,headers,formatted_report)
