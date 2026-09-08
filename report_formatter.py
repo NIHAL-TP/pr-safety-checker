b3d90f2d00a098eedd84be5e8afec2b5396988d4
NIHAL-TP <nihaltp101@gmail.com>
2026-09-08 12:07:40 +0000

    changed wording in at artifact link.1st phase completed

--- report_formatter.py
+++ report_formatter.py
@@ -46,7 +46,7 @@ def report_formatter(findings,count,remaining_count,artifact_url,cap=True):
     table.append(combined_content)
     table = "\n".join(table)
     if cap:
-        table = f"**Summary:** {count['CRITICAL']} CRITICAL, {count['HIGH']} HIGH, {count['MEDIUM']} MEDIUM, {count['LOW']} LOW, {count['UNKNOWN']} UNKNOWN severity issues found.\n Top 50 issues shown below,check {artifact_url} for further details" + "\n" + table
+        table = f"**Summary:** {count['CRITICAL']} CRITICAL, {count['HIGH']} HIGH, {count['MEDIUM']} MEDIUM, {count['LOW']} LOW, {count['UNKNOWN']} UNKNOWN severity issues found.\n Top 50 issues shown below,check artifacts at {artifact_url} for further details" + "\n" + table
     else:
         table = table
     print("length of table is ",len(table))
